4cb4ba456bf859fa59a4e863b2e520bfb99f3e81
sso214 <sojam214@gmail.com>
2023-11-04 09:18:31 +0900

    feat : 모바일 작업 완료

---
+++ ASF/mo/js/custom.js
@@ -0,0 +1,63 @@
+var el = document.querySelectorAll('section');
+var app = new onePageScroll({
+    el: el,
+    easing: 'ease-out',
+    throttling: 300,
+});
+
+$(document).ready(function(){
+    $(".menu-button").on('click', function(){
+        $("nav").addClass('on');
+    });
+    $(".menu-close").on('click', function(){
+        $("nav").removeClass('on');
+    });
+
+    $('nav ul li').on('click', function(){
+        var index = $(this).index();
+        app.goto(index+1);
+        $("nav").removeClass('on');
+    });
+
+    $(".lang button").on('click', function(){
+        $(".lang-list").toggleClass('on');
+    });
+    $('html').click(function(e) {
+        if($(e.target).parents('.lang-button').length < 1 && !$(e.target).hasClass('lang-button')){
+            $(".lang-list").removeClass('on');
+        }
+    });
+    $(".lang-list li").on('click', function(){
+        var name = $(this).text();
+        var value = $(this).data('key');
+
+        $(".lang button span").text(name);
+        console.log(value);
+    });
+
+    function stopVideo() {
+        var visualVideoTarget = $(".visualVideo");
+        var visualVideoSrc = visualVideoTarget.attr('src');
+        visualVideoTarget.attr("src","");
+        visualVideoTarget.attr("src",visualVideoSrc);
+    }
+
+    var popBlack = $("#pop-black");
+    if (popBlack) {
+        var pop = $(".pop");
+        var popOpen = $(".pop-btn");
+        var popClose = $(".pop-close");
+
+        popOpen.on('click', function () {
+            var dataKey = $(this).data('key');
+            $(".pop." + dataKey).addClass('on');
+            popBlack.addClass('on');
+        });
+
+        popClose.on('click', function() {
+            pop.removeClass('on');
+            popBlack.removeClass('on');
+            stopVideo();
+        });
+    }
+});
