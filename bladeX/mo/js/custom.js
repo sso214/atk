9d24954fb02a97c33e4a55e1dce9a5de6748b3f5
sso214 <sojam214@gmail.com>
2023-11-25 13:59:37 +0900

    feat : bladeX MO

---
+++ bladeX/mo/js/custom.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+    $(".lang button").on('click', function(){
+        $(".lang-list").toggleClass('on');
+    });
+    $('html').click(function(e) {
+        if($(e.target).parents('.lang-button').length < 1 && !$(e.target).hasClass('lang-button')){
+            $(".lang-list").removeClass('on');
+        }
+    });
+
+    function getLangSrc(target) {
+        target.attr('src', `img/${$('#meta-lang').attr('content')}/${target.data('img')}`);
+    }
+    $(".lang-list li").on('click', function(){
+        var name = $(this).text();
+        var lang = $(this).data('key');
+        var isLangKo = lang === 'ko';
+
+        $(".lang button span").text(name);
+        $('#meta-lang').attr('content', lang);
+
+        getLangSrc($('.title'));
+        getLangSrc($('.input-form'));
+        getLangSrc($('.guide01'));
+        getLangSrc($('.guide-button'));
+        getLangSrc($('.hidden-guide'));
+
+        $('#uid').attr('placeholder', isLangKo ? 'UID를 입력해주세요' : 'Please enter your UID');
+        $('#coupon').attr('placeholder', isLangKo ? '쿠폰번호를 입력해주세요' : 'Please enter your coupon number');
+
+        getLangSrc($('.pop.empty-uid img'));
+        getLangSrc($('.pop.empty-coupon img'));
+        getLangSrc($('.pop.not-match-uid img'));
+        getLangSrc($('.pop.not-match-coupon img'));
+        getLangSrc($('.pop.over-count img'));
+        getLangSrc($('.pop.already-coupon img'));
+        getLangSrc($('.pop.not-duplicate-use img'));
+        getLangSrc($('.pop.expiration img'));
+        getLangSrc($('.pop.invalid-coupon img'));
+        getLangSrc($('.pop.server-unavailable img'));
+        getLangSrc($('.pop.market-unavailable img'));
+        getLangSrc($('.pop.same-coupone-over-count img'));
+        getLangSrc($('.pop.db-error img'));
+        getLangSrc($('.pop.success img'));
+    });
+
+    var popBlack = $("#pop-black");
+    if (popBlack) {
+        var pop = $(".pop");
+        var popOpen = $(".pop-btn");
+
+        popOpen.on('click', function () {
+            var dataKey = $(this).data('key');
+            $(".pop." + dataKey).addClass('on');
+            popBlack.addClass('on');
+        });
+
+        $('#pop-black').on('click', '.pop-close', function() {
+            pop.removeClass('on');
+            popBlack.removeClass('on');
+        })
+
+        popBlack.on('click', function (e) {
+            if (e.target.id == "pop-black") {
+                pop.removeClass('on');
+                popBlack.removeClass('on');
+            }
+        });
+    }
+
+
+    function popOpenEvt(popName){
+        $(`.pop.` + popName).addClass('on');
+        $("#pop-black").addClass('on');
+    }
+
+    $('.input-box button').on('click', function () {
+        var regExpUid = /[0-9]/;
+        var regExpCoupon = /[0-9]/;
+
+        if (!$('input#uid').val()) return popOpenEvt('empty-uid');
+        if (!$('input#coupon').val()) return popOpenEvt('empty-coupon');
+        if (!regExpUid.test($("input#uid").val())) return popOpenEvt('not-match-uid');
+        if (!regExpCoupon.test($("input#coupon").val())) return popOpenEvt('not-match-coupon');
+        // if (쿠폰 사용 횟수 초과 시) return popOpenEvt('over-count');
+        // if (이미 등록된 쿠폰일 시) return popOpenEvt('already-coupon');
+        // if (중복 사용 쿠폰일 시) return popOpenEvt('not-duplicate-use');
+        // if (기간 만료 쿠폰일 시) return popOpenEvt('expiration');
+        // if (쿠폰 정보가 잠롯됐을 시) return popOpenEvt('invalid-coupon');
+        // if (쿠폰 사용 가능한 서버가 아닐 시) return popOpenEvt('server-unavailable');
+        // if (쿠폰 사용 가능한 마켓이 아닐 시) return popOpenEvt('market-unavailable');
+        // if (같은 종류의 쿠폰 사용 횟수를 초과했을 시) return popOpenEvt('same-coupone-over-count');
+        // if (쿠폰 사용 중 DB 오류가 발생했을 시) return popOpenEvt('db-error');
+        popOpenEvt('success');
+    });
+
+    $(".guide-button").on('click', function (){
+       $('.hidden-guide').stop().slideToggle(200);
+    });
+});
